c75b933fe5228953c76259934e2d27cbedcde574
WtoJglg4 <glazovv81@gmail.com>
2023-10-23 14:04:03 +0300

    Restructured and added some functions

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@
 #include <chrono>
 #include <limits.h>
 using namespace std;
+using namespace chrono;
 
 //CONSTANTS
-const int   MIN_INT = -100,
-            MAX_INT = 100,
-            LENGTH = 200;
+const int   MIN_INT = -1000,
+            MAX_INT = 1000,
+            LENGTH = 200000, 
+            FILES_QUANTITY = 4;
 
 //SEARCHING
-int BetterLinearSearch(int[], int, int);
-int SentinelLinearSearch(int[], int, int);
-int OrderedArraySearch(int[], int, int);
-int BinarySearch(int[], int, int, int);
+int BetterLinearSearch(int[], int, int, int&);
+int SentinelLinearSearch(int[], int, int, int&);
+int OrderedArraySearch(int[], int, int, int&);
+int BinarySearch(int[], int, int, int&);
+int(*Funcs[])(int[], int, int, int&){BetterLinearSearch, SentinelLinearSearch, OrderedArraySearch, BinarySearch};
 
 //VARIABLES
 int Array[LENGTH];
@@ -23,46 +26,69 @@ int Array[LENGTH];
 void AscendingSequenceInt(int[], int);
 void RandomSequenceInt(int[], int);
 
-//PRINTING SEQUENCES
-void PrintArrConsoleInt(int[], int); 
-void PrintArrFileInt(int[], int, FILE*, int64_t);
-
-//POINTERS INTEGER FUNCTIONS
-void (*funcInt1)(int[], int) = AscendingSequenceInt;
-void (*funcInt3)(int[], int) = RandomSequenceInt;
+//PRINTING RESULTS
+void ClearAllFiles(int);
+void PrintResFile(int, int, int64_t, int, FILE*);
 
 //ERROR HANDLERS
 int ErrorHandler(int, int, double, double, int, int);
 void PrintError(int);
 
 //FILES
+const char* Files[] = {"output/BLS.txt", "output/SLS.txt", "output/OAS.txt", "output/BS.txt"};
 
 int main(){
-    // int Array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    // int Array2[10] = {172, 123, 8, 12, 0, 122, 81, 20, 22, 10};
-    // int len = 10;
-    // cout << BetterLinearSearch(Array, len, 2) << endl;
-    // cout << SentinelLinearSearch(Array, len, 2) << endl;
-    // cout << OrderedArraySearch(Array, len, 2) << endl;
-    // cout << BinarySearch(Array, 2, 0, 9) << endl;
+    ClearAllFiles(FILES_QUANTITY);
 
-    // cout << BetterLinearSearch(Array, len, 11) << endl;
-    // cout << SentinelLinearSearch(Array, len, 11) << endl;
-    // cout << OrderedArraySearch(Array, len, 11) << endl;
-    // cout << BinarySearch(Array, 11, 0, 9) << endl;
+    int target = MAX_INT + 1;
+    for(int len = 10000; len <= 200000; len = len + 19000){
+        RandomSequenceInt(Array, len);
+        for(int j = 0; j < 4; j++){
+            if (j == 2){
+                AscendingSequenceInt(Array, len);
+            }
+            FILE* file = fopen(Files[j], "a+");
+            int temp = target;
+            
+            int comparations = -1;
+            swap(Array[0], temp);
+            auto begin = steady_clock::now();
+            int res = Funcs[j](Array, len, target, comparations);
+            auto end = steady_clock::now();
+            swap(Array[0], temp);
+            auto duration = duration_cast<microseconds>(end - begin);
+            PrintResFile(res, len, duration.count(), 0, file);
+            
+            comparations = -1;
+            swap(Array[len/2], temp);
+            begin = steady_clock::now();
+            res = Funcs[j](Array, len, target, comparations);
+            end = steady_clock::now();
+            swap(Array[len/2], temp);
+            duration = duration_cast<microseconds>(end - begin);
+            PrintResFile(res, len, duration.count(), 0, file);
+
+            comparations = -1;
+            swap(Array[len-1], temp);
+            begin = steady_clock::now();
+            res = Funcs[j](Array, len, target, comparations);
+            end = steady_clock::now();
+            swap(Array[len-1], temp);
+            duration = duration_cast<microseconds>(end - begin);
+            PrintResFile(res, len, duration.count(), 0, file);
+            fprintf(file, "\n");
 
 
-    // cout << BetterLinearSearch(Array2, len, 22) << endl;
-    // cout << SentinelLinearSearch(Array2, len, 22) << endl;
-
-    
-    // cout << BetterLinearSearch(Array2, len, 222) << endl;
-    // cout << SentinelLinearSearch(Array2, len, 222) << endl;
+            fclose(file);
+        }
+        
+    }
 }
 
 //searching
-int BetterLinearSearch(int Array[], int len, int target){
+int BetterLinearSearch(int Array[], int len, int target, int &comparations){
     for (int i = 0; i < len; i++){
+        comparations += 2;
         if (Array[i] == target){
             return i;
         }
@@ -70,12 +96,15 @@ int BetterLinearSearch(int Array[], int len, int target){
     return -1;
 }
 
-int SentinelLinearSearch(int Array[], int len, int target){
+int SentinelLinearSearch(int Array[], int len, int target, int &comparations){
     int lastInd = len - 1, last = Array[lastInd], i = 0;
     Array[lastInd] = target;
     while (Array[i] != target){
+        comparations++;
         i++;
     }
+    comparations += 2; //последнее сравнение, когда в цикл не заходим + сравнение в if далее
+
     Array[lastInd] = last;
     if (i < lastInd || Array[lastInd] == target){
         return i;
@@ -83,7 +112,7 @@ int SentinelLinearSearch(int Array[], int len, int target){
     return -1;
 }
 
-int OrderedArraySearch(int Array[], int len, int target){
+int OrderedArraySearch(int Array[], int len, int target, int &comparations){
     int lastInd = len - 1, last = Array[lastInd], i = 0;
     Array[lastInd] = INT32_MAX;
     while (target > Array[i]){
@@ -96,8 +125,8 @@ int OrderedArraySearch(int Array[], int len, int target){
     return -1;
 }
 
-int BinarySearch(int Array[], int target, int left, int right){
-    int answer = -1;
+int BinarySearch(int Array[], int target, int len, int &comparations){
+    int answer = -1, left = 0, right = len - 1;
     while(left < right){
         int m = (left + right) / 2;
         if (Array[m] < target){
@@ -115,6 +144,8 @@ int BinarySearch(int Array[], int target, int left, int right){
 //error handler
 int ErrorHandler(int minInt, int maxInt, double minDouble, double maxDouble, int length, int intervalLength){
     if (maxInt <= minInt) return 1;
+
+
     if (maxDouble <= minDouble) return 2;
     if (length < 1) return 3;
     if (length < intervalLength) return 4;
@@ -144,33 +175,17 @@ void PrintError(int error){
     }
 }
 
-
-//print int array to console
-void PrintArrConsoleInt(int Array[], int len){
-    if (len >= 100000){
-        return;
-    }
-    for (int i = 0; i < len; i++){
-        char end[3];
-        if (i != len - 1){
-            strcpy(end, ", ");
-        } else{
-            strcpy(end, "]\n");
-        }
-        printf("%d%s", Array[i], end);
-    }
+//print result to file
+void PrintResFile(int res, int len, int64_t time, int comparations, FILE *file){
+    fprintf(file, "%d %d %d %d\n", res, len, time, comparations);
 }
 
-//print int array to file
-void PrintArrFileInt(int Array[], int len, FILE *file, int64_t time){
-    fprintf(file, "Время работы алгоритма по формированию последовательности: %d(мкС)\n", time);
-    if (len >= 100000){
-        return;
-    }
+//clear all files
+void ClearAllFiles(int len){
     for (int i = 0; i < len; i++){
-        fprintf(file, "%d\n", Array[i]);
+        FILE *file = fopen(Files[i], "w");
+        fclose(file);
     }
-    fprintf(file, "\n");
 }
 
 //generates an ascending integer sequence
